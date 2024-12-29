3d2d33488b2c7c4ef634a204971a6b4d09b3a4a6
Thullyoo <thullyofilho@gmail.com>
2024-12-29 18:32:20 -0300

    feat: add send user registered notification on save user

--- user-producer-kafka/src/main/java/br/thullyoo/user_producer_kafka/service/UserService.java
+++ user-producer-kafka/src/main/java/br/thullyoo/user_producer_kafka/service/UserService.java
@@ -1,6 +1,8 @@
 package br.thullyoo.user_producer_kafka.service;
 
+import br.thullyoo.user_producer_kafka.DTO.UserProducerDTO;
 import br.thullyoo.user_producer_kafka.DTO.UserRequest;
+import br.thullyoo.user_producer_kafka.producer.UserProducer;
 import br.thullyoo.user_producer_kafka.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
@@ -8,14 +10,17 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final UserProducer userProducer;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, UserProducer userProducer) {
         this.userRepository = userRepository;
+        this.userProducer = userProducer;
     }
 
     public void saveUser(UserRequest userRequest){
 
         userRepository.save(userRequest);
 
+        userProducer.sendUserRegisteredNotification(new UserProducerDTO(userRequest.email(), userRequest.username()));
     }
 }
